368c3a3d25ca844ea7ee2c09bf9acc3f3c1293bc
manu-garcia <garcia.martin.manu@gmail.com>
2020-04-01 22:50:35 +0200

    Changes base url

--- docs/main.b0a109ad.js
+++ docs/main.b0a109ad.js
@@ -8242,7 +8242,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61489" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62807" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -8419,4 +8419,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["../../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js","src/main.ts"], null)
-//# sourceMappingURL=/main.b0a109ad.js.map
+//# sourceMappingURL=/stupidduck/main.b0a109ad.js.map
